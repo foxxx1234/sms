3282131e93599fb7ac3538d68c44ce7f578dcc42
firefox.fun <57606711+foxxx1234@users.noreply.github.com>
2025-07-08 22:34:32 +0300

    Allow context menu after disconnect

--- static/js/contextMenu.js
+++ static/js/contextMenu.js
@@ -117,7 +117,6 @@ document.addEventListener("click", e => {
 
 // Перехватываем контекстное меню на строках таблицы
 document.addEventListener("contextmenu", e => {
-  if (window.scriptsDisabled) return;
   const row = e.target.closest("tr[data-port]");
   if (!row) return;
   e.preventDefault();
